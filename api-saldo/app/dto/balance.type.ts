ec06f7ab19dc56349bf089dc5dac9bf182483475
Paulo Vaz <pvaz1006@gmail.com>
2024-06-19 00:23:44 -0300

    feat: add types in balance api

---
+++ api-saldo/app/dto/balance.type.ts
@@ -0,0 +1,4 @@
+export type TBalance = {
+  user_id?: string;
+  amount?: number;
+}
---
+++ api-saldo/app/dto/index.ts
@@ -0,0 +1,7 @@
+import { HTTPStatus } from "./HTTPStatus.enum";
+import { TBalance } from "./balance.type";
+
+export {
+  HTTPStatus,
+  TBalance
+}
