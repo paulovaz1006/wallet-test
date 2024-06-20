becd6abd8fb3114f60a249e6a94a07eae2951917
Paulo Vaz <pvaz1006@gmail.com>
2024-06-20 11:13:31 -0300

    chore: config migration

--- api-extrato/app/database/migrations/1718812509427-AddTypeTransaction.ts
+++ api-extrato/app/database/migrations/1718812509427-AddTypeTransaction.ts
@@ -4,20 +4,17 @@ export class AddTypeTransaction1718812509427 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            INSERT INTO types_transaction (id, type)
-            SELECT * FROM (
-                SELECT '1' as id, 'compras' as type
-                UNION ALL SELECT '2', 'cancelamento'
-                UNION ALL SELECT '3', 'estorno'
-                UNION ALL SELECT '4', 'adição de valores'
-                UNION ALL SELECT '5', 'retirada de valores'
-            ) AS new_types
-            WHERE NOT EXISTS (
-                SELECT 1 FROM types_transaction WHERE id = new_types.id
+            INSERT INTO types_transaction (id, type) values
+            ('1', 'compras'),
+            ('2', 'cancelamento'),
+            ('3', 'estorno'),
+            ('4', 'adição de valores'),
+            ('5', 'retirada de valores')
         `)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`TRUNCATE TABLE types_transaction`)
     }
 
 }
