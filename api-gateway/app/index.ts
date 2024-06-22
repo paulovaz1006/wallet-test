6a132614c85d06e7f10aba9a2d02a7b01b45a336
Paulo Vaz <pvaz1006@gmail.com>
2024-06-22 02:23:23 -0300

    fix: adjust index

--- api-gateway/app/index.ts
+++ api-gateway/app/index.ts
@@ -4,7 +4,6 @@ import server from "./server";
 const port = process.env.PORT || 3000;
  
 server.listen(port, () => {
-    console.log(`Run server api-gateway in http://localhost:${port}`)
-
     swaggerDocs(server, Number(port))
+    console.log(`Run server api-gateway in http://localhost:${port}`)    
 })
