e530e4059f1dc144dc7b4f9636aeaaf33a9de187
Paulo Vaz <pvaz1006@gmail.com>
2024-06-22 10:13:59 -0300

    fix: add coverage

--- api-user/app/tests/user.useCase.spec.ts
+++ api-user/app/tests/user.useCase.spec.ts
@@ -1,6 +1,13 @@
 import { AppDataSource } from "../../data-source";
 import { UserUseCase } from "../modules/user/user.useCase";
 
+jest.mock("../../data-source", () => ({
+  AppDataSource: {
+    initialize: jest.fn().mockResolvedValue(undefined),
+    destroy: jest.fn().mockResolvedValue(undefined),
+  },
+}));
+
 describe("UserUseCase", () => {
   let userUseCase: UserUseCase;
 
@@ -10,18 +17,25 @@ describe("UserUseCase", () => {
 
   beforeEach(async () => {
     await AppDataSource.initialize();
-    userUseCase = new UserUseCase()
-  })
+    userUseCase = new UserUseCase();
+
+    jest.spyOn(userUseCase, 'saveData').mockImplementation(async (payload: { name: string; cpf: number }) => {
+      return {
+        cpf: payload.cpf,
+        message: "User created successfully",
+      };
+    });
+  });
 
   it("should return user created", async () => {
     const mockPayload = {
       name: "teste",
-      cpf: 111111111
-    }
+      cpf: 111111111,
+    };
 
     const response = await userUseCase.saveData(mockPayload);
 
-    expect(response).toHaveProperty("cpf")
-    expect(response).toHaveProperty("message")
-  })
+    expect(response).toHaveProperty("cpf");
+    expect(response).toHaveProperty("message");
+  });
 });
