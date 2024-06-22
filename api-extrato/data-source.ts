27b85254a437a8822cb6d67be1105aecfa12693c
Paulo Vaz <pvaz1006@gmail.com>
2024-06-22 03:23:09 -0300

    fix: adjust data-source

--- api-extrato/data-source.ts
+++ api-extrato/data-source.ts
@@ -2,14 +2,12 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 
 export const AppDataSource = new DataSource({
-    // type: "postgres",
-    // host: "localhost",
-    // port: 5434,
-    // username: "apiextrato",
-    // password: "apiextrato",
-    type: "sqlite",
-    database: "./app/database/bank_statement.sqlite",
-    // database: "extrato",
+    type: "postgres",
+    host: "postgres_apiextrato",
+    port: 5434,
+    username: "apiextrato",
+    password: "apiextrato",
+    database: "extrato",
     entities: ["./app/entity/*.ts"],
     migrations: ["./app/database/migrations/*.ts"],
     synchronize: true
--- api-saldo/data-source.ts
+++ api-saldo/data-source.ts
@@ -3,14 +3,12 @@ import { DataSource } from "typeorm"
 import AccountsEntity from "./app/entity/Accounts.entity"
 
 export const AppDataSource = new DataSource({
-    // type: "postgres",
-    // port: 5433,
-    // username: "apisaldo",
-    // password: "apisaldo",
-    // database: "saldo",
-    // host: "localhost",
-    type: "sqlite",
-    database: "./app/database/balance.sqlite",
+    type: "postgres",
+    host: "postgres_apisaldo",
+    port: 5433,
+    username: "apisaldo",
+    password: "apisaldo",
+    database: "saldo",
     entities: [AccountsEntity],
     synchronize: true   
 })
